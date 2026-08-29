90fc860c522e276a306f54c59adc5ee6a044174d
KevinFu <fukan211@gmail.com>
2026-08-29 16:33:45 +0800

    fix: link nest and postgres

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,43 @@
 import { Module } from '@nestjs/common';
+import { ConfigService, ConfigModule } from '@nestjs/config';
+import * as path from 'path';
+import postgresConfig, { PostgresConfig } from './config/postgres.config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HealthController } from './health/health.controller';
 import { HealthModule } from './health/health.module';
+import { DocumentsController } from './documents/documents.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { HealthController } from './health/health.controller';
 
 @Module({
-  imports: [HealthModule],
-  controllers: [AppController, HealthController],
+  imports: [
+    HealthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: path.resolve(__dirname, '../../../.env'),
+      load: [postgresConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const postgres = config.get<PostgresConfig>('postgres');
+        if (!postgres) {
+          throw new Error('Postgres config not found');
+        }
+        return {
+          type: 'postgres' as const,
+          host: postgres.host,
+          port: postgres.port || 5432,
+          username: postgres.username,
+          password: postgres.password,
+          database: postgres.database,
+          autoLoadEntities: true,
+          synchronize: false,
+        };
+      },
+    }),
+  ],
+  controllers: [AppController, HealthController, DocumentsController],
   providers: [AppService],
 })
 export class AppModule {}
